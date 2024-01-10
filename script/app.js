5ed317615a348f849c7514b8ca8c57fddab81c14
MidoA92 <87500895+MidoA92@users.noreply.github.com>
2024-01-10 23:03:51 +0100

    update

--- script/app.js
+++ script/app.js
@@ -1,14 +1,44 @@
+'use strict';
 const btnBook = document.querySelector('.btn-books');
 const btnUser = document.querySelector('.btn-user');
 const infoBooks = document.getElementById('infoBooks');
 const infoUser = document.getElementById('infoUser');
+const booksList = document.querySelector('.booksList');
+const usersList = document.querySelector('.usersList');
+const input = document.getElementsByTagName('input');
+const inputValue = input.value;
+const addBook = document.querySelector('.addBook');
+const addUser = document.querySelector('.addUser');
 
 function removeVisible() {
-  infoBooks.classList.toggle('invisble');
-  infoUser.classList.toggle('invisble');
-  btnBook.classList.toggle('invisble');
-  btnUser.classList.toggle('invisble');
+  infoBooks.classList.remove('invisble');
+  infoUser.classList.remove('invisble');
+  btnBook.classList.add('invisible');
+  btnUser.classList.add('invisible');
 }
 
+let books = [];
+
+const addBooks = (ev) => {
+  ev.preventDefault();
+  let book = {
+    code: document.getElementById('code').value,
+    title: document.getElementById('title').value,
+    nameAuthor: document.getElementById('nameAuthor').value,
+    yearPub: document.getElementById('yearPub').value,
+    placePub: document.getElementById('placePub').value,
+    publisher: document.getElementById('publisher').value,
+  };
+  console.log(book);
+  books.push(book);
+};
+
+function inputValues() {
+  for (let book of books) {
+    console.log(book);
+  }
+}
 btnBook.addEventListener('click', removeVisible);
 btnUser.addEventListener('click', removeVisible);
+addBook.addEventListener('click', addBooks);
+addUser.addEventListener('click', inputValues);
